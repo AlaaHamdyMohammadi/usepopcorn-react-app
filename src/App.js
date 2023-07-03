e9de3d737ca543033f01d6fae61585993decfe62
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-07-03 18:21:13 +0200

    pUpdate

--- src/App.js
+++ src/App.js
@@ -95,12 +95,15 @@ export default function App() {
 
   useEffect(
     function () {
+
+      const controller = new AbortController(); //Browser API
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`, {signal: controller.signal}
           );
 
           if (!res.ok)
@@ -110,19 +113,27 @@ export default function App() {
           if (data.Response === "False") throw new Error("Movie not found");
 
           setMovies(data.Search);
+          setError('');
         } catch (err) {
           //console.error(err.message);
-          setError(err.message);
+
+          if(err.name !== 'AbortError'){
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
       }
-      if (!query.length) {
+      if (!query.length < 3) {
         setMovies([]);
         setError("");
         return;
       }
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      }
     },
     [query]
   );
